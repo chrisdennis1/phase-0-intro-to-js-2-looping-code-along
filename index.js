3620fc838b823ac29359f136a0045c3039bd8c5a
chrisdennis12345@gmail.com <chrisdennis@Chriss-iMac.local>
2022-08-09 23:53:55 -0700

    commit tree 4c324419c733e278cfd2f049d5b759f70d1575eb

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = ['Guadalupe', 'Ollie', 'Aki'];
+
+function writeCards(names) {
+    let array = []
+  for (let i = 0; i < names.length; i++) {
+    array.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+  return array 
+
+}
+
+writeCards('Guadalupe', 'Ollie', 'Aki');
+
+let number=10
+function countDown(number)
+ {
+while (number > -1){ 
+  console.log(number--);
+}}
